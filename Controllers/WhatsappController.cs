d65ccd20fcd89b865a58de6a2ee432703e609dad
ali.cruz <ali.cruz@me.com>
2023-09-17 19:19:52 -0600

    API GET para responder (sample), API para Token Acceso Video 17

---
+++ Controllers/WhatsappController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NetCore_WhatsApp_1.Controllers
+{
+    [ApiController]
+    [Route("api/whatsapp")]
+    public class WhatsappController : Controller
+    {
+        [HttpGet("test")]
+        public IActionResult Sample()
+        {
+            return Ok("Ok Sample");
+        }
+
+        [HttpGet]
+        public IActionResult VerifyToken()
+        {
+            string accessToken = "DFGDFGRFGFGHFGH567345DFGDFG2345DFGHDFGH456DFGH";
+            var token = Request.Query["hub.verify_token"].ToString();
+            var challenge = Request.Query["hub.challenge"].ToString();
+
+            if (challenge != null && token != null && token == accessToken)
+            {
+                return Ok(challenge);
+            } else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ReceivedMessage([FromBody] )
+        {
+
+        }
+
+    }
+}
---
+++ Models/WhatsappCloud/WhatsappCloudModel.cs
@@ -0,0 +1,53 @@
+﻿namespace ASP.NetCore_WhatsApp_1.Models.WhatsappCloud
+{
+    public class WhatsappCloudModel
+    {
+        public List<Entry> Entry { get; set; }
+    }
+    public class Entry
+    {
+        public List<Change>? Changes { get; set; }
+    }
+    public class Change
+    {
+        public Value? Value { get; set; }
+        public string? Field { get; set; }
+    }
+    public class Value
+    {
+        public string? Messaging_Product { get; set; }
+        public List<Message>? Messages { get; set; }
+    }
+    public class Message
+    {
+        public string? From { get; set; }
+        public string? Id { get; set; }
+        public string? Timestamp { get; set; }
+        public string? Type { get; set; }
+        public Text? Text { get; set; }
+        public Interactive? Interactive { get; set; }
+
+    }
+    public class Interactive
+    {
+        public string? Type { get; set; }
+        public ListReply? List_Reply { get; set; }
+        public ButtonReply? Button_Reply { get; set; }
+    }
+    public class ListReply
+    {
+        public string? Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+    }
+    public class ButtonReply
+    {
+        public string? Id { get; set; }
+        public string? Title { get; set; }
+    }
+
+    public class Text
+    {
+        public string? Body { get; set; }
+    }
+}
